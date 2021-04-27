87b671ebb67bc3c228637c64b5d62c5b682f1b46
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-26 20:46:51 -0600

    Two way biding and form added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,37 @@
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import Button from '../UI/Button';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  //Variable States
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
   //Handle the add user event
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  //Handle the userName entered
+  const userNameEnteredHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  //Handle the userName entered
+  const ageEnteredHandler = (event) => {
+    setUserAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameEnteredHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageEnteredHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
