558ef00d1b6e1987c896ce388e376f0213cd5ee4
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-26 20:35:16 -0600

    btn component added

---
+++ src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    ></button>
+  );
+};
+
+export default Button;
