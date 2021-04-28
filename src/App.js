72e6a86d3b5a70a083982f19bf4c0045c91ebc5a
filipecamargos <filipe.atrabalho@gmail.com>
2021-04-27 19:07:09 -0600

    Wrapper implemented to appjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
+import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
   //Add a user to the list @user {name & age}
-  const userListHandler = user => {
+  const userListHandler = (user) => {
     setUsersList((preList) => {
       return [...preList, user];
     });
-  }
+  };
 
   return (
-    <div>
-      <AddUser onAddUser={userListHandler}/>
+    <Wrapper>
+      <AddUser onAddUser={userListHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Wrapper>
   );
 }
 
